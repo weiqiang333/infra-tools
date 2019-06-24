e501000aedac6a9f5331b247fee828e4f01f24df
weiqiang <weiqiang@growingio.com>
2019-06-24 13:57:33 +0800

    add conn check

--- init/build.sh
+++ init/build.sh
@@ -4,4 +4,5 @@ export GOARCH=amd64
 export GOOS=linux
 export GCCGO=gc
 
-go build infra-tools.go
+go build -ldflags "-s -w"  -o ./bin/infra-tools infra-tools.go
+go build -ldflags "-s -w"  -o ./bin/check cmd/check/check.go
